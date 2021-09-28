693b032d8d8375d954a578497833baa1475c4632
amarpadilah <mobpsyco123@gmail.com>
2021-09-28 13:57:16 +0700

    menambahkan commit file

---
+++ application/controllers/CONTOH1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Amar padilah <p> 
+        Saya tinggal dikecamatan sindangkasih kabupaten ciamis <p> 
+        Olahraga yang saya sukai adalah bola voly <p> 
+        Nomor hp saya 08123456789 <p>";
+
+    }
+}
